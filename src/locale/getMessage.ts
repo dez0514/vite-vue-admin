2ae4af26011812f845d8bbd210b2a15c68ea97cc
zhangwd24 <2573433803@qqcom>
2023-08-17 09:40:39 +0800

    top level await

--- src/locale/getMessage.ts
+++ src/locale/getMessage.ts
@@ -1,15 +1,19 @@
-let modules = {}
-async function getLocal() {
-  const modulesFiles = import.meta.glob( './language/**/*.ts' ) as Record<string, () => Promise<any>>;
-  for (const path in modulesFiles) {
-    const moduleName = path.replace( /(.*\/)*([^.]+).*/gi, '$2' )
-    const moduleDefaultExport = await modulesFiles[path]()
-    modules = {
-      ...modules,
-      [moduleName] : moduleDefaultExport.default
-    }
-  }
-}
-getLocal()
+import zh from './language/zh'
+import en from './language/en'
+
+// async function getLocal() {
+//   let modules = {}
+//   const modulesFiles = import.meta.glob( './language/**/*.ts' ) as Record<string, () => Promise<any>>;
+//   for (const path in modulesFiles) {
+//     const moduleName = path.replace( /(.*\/)*([^.]+).*/gi, '$2' )
+//     const moduleDefaultExport = await modulesFiles[path]()
+//     modules = {
+//       ...modules,
+//       [moduleName] : moduleDefaultExport.default
+//     }
+//   }
+//   return modules
+// }
+const modules = { zh, en } // getLocal()
 
 export default modules
--- src/mockProdServer.ts
+++ src/mockProdServer.ts
@@ -1,17 +1,18 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
-// import userMock from '../mock/user'
-// import homeMock from '../mock/home'
-let mockModules: any = []
-async function creatMock() {
-  const modulesMockFiles = import.meta.glob('../mock/*.ts') as Record<string, () => Promise<any>>;
-  for (const path in modulesMockFiles) {
-    const moduleDefaultExport = await modulesMockFiles[path]()
-    mockModules.push(moduleDefaultExport.default)
-  } 
-}
-creatMock()
+import userMock from '../mock/user'
+import homeMock from '../mock/home'
+// async function creatMock() {
+//   let mockModules: any = []
+//   const modulesMockFiles = import.meta.glob('../mock/*.ts') as Record<string, () => Promise<any>>;
+//   for (const path in modulesMockFiles) {
+//     const moduleDefaultExport = await modulesMockFiles[path]()
+//     mockModules.push(moduleDefaultExport.default)
+//   }
+//   return mockModules
+// }
 
-export async function setupProdMockServer(): Promise<void> {
-  const mockModulesData = [...mockModules]
+export function setupProdMockServer() {
+  // const mockModules = await creatMock()
+  const mockModulesData = [userMock, homeMock]
   createProdMockServer(mockModulesData)
 }
